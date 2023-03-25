38929779649bceb6131c77cc1c99598c72f8534b
JanicaJensen <122334098+JanicaJensen@users.noreply.github.com>
2023-03-25 14:12:26 -0600

    fixed a local storage bug and added screenshots to readme

--- script.js
+++ script.js
@@ -1,58 +1,65 @@
 
 $(function () {
-  //Sets the date at the top of the page and the military time 
+  //Sets the date and time at the top of the page
   var today = dayjs();
   $('#currentDay').text(today.format('MMM D, YYYY'));
   var currentTime = dayjs();
-  $("#currentTime").text(currentTime.format('HH : mm A'));
+  $("#currentTime").text(currentTime.format('hh : mm A'));
   
- //sets an event listener on the .saveBtn class
-  $(".saveBtn").on ("click", function() {
-    //creates variables for the user input in the time block description sections and the time in the time block
-    var text = $(this).siblings(".description").val(); 
-    var time = $(this).parent().attr("id");
-   //sets items to local storage in format of time, text.
-    localStorage.setItem(time, text);
-  })
- //retrieves the elements that match the id hour#, must use military time numbers for easier comparison.
-  $("#hour8.description").val(localStorage.getItem("hour8"));
-  $("#hour9.description").val(localStorage.getItem("hour9"));
-  $("#hour10.description").val(localStorage.getItem("hour10"));
-  $("#hour11.description").val(localStorage.getItem("hour11"));
-  $("#hour12.description").val(localStorage.getItem("hour12"));
-  $("#hour13.description").val(localStorage.getItem("hour13"));
-  $("#hour14.description").val(localStorage.getItem("hour14"));
-  $("#hour15.description").val(localStorage.getItem("hour15"));
-  $("#hour16.description").val(localStorage.getItem("hour16"));
-  $("#hour17.description").val(localStorage.getItem("hour17"));
- 
-  //the function that comares the current time vs time of the description text is below
+  $("#clearBtn").click(function (event) {
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
+  });
+
   function hourCompare() {
-  //sets the current hour variable using dayJS
-    var currentHour = dayjs().format('HH'); 
-    //takes the ID of the time block and removes hour from it for comparison with our current hour
-    $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(blockHour, currentHour)
-/*assigns classes based on the equality of the times. I wasn't sure if I needed to leave in the 
-remove/remove because I suppose if you leave the page open long enough, eventually multiple classes could be
-applied to one time-block, so I left the removal in for redundancy. */
-    if (blockHour < currentHour) {
-      $(this).addClass("past");
-      $(this).removeClass("future");
-      $(this).removeClass("present");
-    }
-    else if (blockHour == currentHour) {
-      $(this).removeClass("past");
-      $(this).addClass("present");
-      $(this).removeClass("future");
+    //sets the current hour variable using dayJS, this is for comparison purposes to change classes of past, present, future.
+      var currentHour = dayjs().format('HH'); 
+      //takes the ID of the time block and removes hour from it for comparison with our current hour
+      $(".time-block").each(function () {
+          var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+          console.log(blockHour, currentHour)
+          if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+          }
+          else if (blockHour == currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+          
+          }
+          else { 
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+          }}
   
-    }
-    else { 
-      $(this).removeClass("present");
-      $(this).removeClass("past");
-      $(this).addClass("future");
-    }})}
-    //runs the function detailed above
-    hourCompare();
+ ) }
+
+
+//runs the function detailed above
+hourCompare();
+ //sets an event listener on the .saveBtn class and assigns key to local storage
+ $(".saveBtn").click(function (event) {
+  event.preventDefault();
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id").split("hour")[1];
+  localStorage.setItem(time, value);
+});
+
+
+ 
+  //this is populating the local storage into the time-blocks.
+ $("#8").val(localStorage.getItem("8"));
+$("#9").val(localStorage.getItem("9"));
+$("#10").val(localStorage.getItem("10"));
+ $("#11").val(localStorage.getItem("11"));
+ $("#12").val(localStorage.getItem("12"));
+ $("#13").val(localStorage.getItem("13"));
+ $("#14").val(localStorage.getItem("14"));
+ $("#15").val(localStorage.getItem("15"));
+ $("#16").val(localStorage.getItem("16"));
+ $("#17").val(localStorage.getItem("17"));
   })
